9f787c9043767c891a345789556b25b7ac68676a
Egor <methodx@aylium.net>
2015-05-28 14:23:54 +0300

    ini_set fix

--- scripts/bitrixenv5_fixes.sh
+++ scripts/bitrixenv5_fixes.sh
@@ -7,6 +7,7 @@ mysql_tzinfo_to_sql /usr/share/zoneinfo | mysql -u root mysql
 
 # Time between system and database fix
 sed -i 's|define("DBPersistent"|date_default_timezone_set("Europe/Moscow");\n&|' /home/bitrix/www/bitrix/php_interface/dbconn.php
+sed -i 's|define("DBPersistent"|ini_set("memory_limit", "512M")\n&|' /home/bitrix/www/bitrix/php_interface/dbconn.php
 sed -i "s|.*?>.*|\$DB->Query(\"SET LOCAL time_zone='Europe/Moscow'\");\n& |" /home/bitrix/www/bitrix/php_interface/after_connect.php
 sed -i "s|.*?>.*|\$connection->queryExecute(\"SET LOCAL time_zone='Europe/Moscow'\");\n&|" /home/bitrix/www/bitrix/php_interface/after_connect_d7.php
 
